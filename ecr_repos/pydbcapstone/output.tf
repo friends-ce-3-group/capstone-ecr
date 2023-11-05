242a4f4b785969584a952407f3e098c8702111ea
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-05 19:22:22 +0800

    try writing to file

--- ecr_repos/pydbcapstone/output.tf
+++ ecr_repos/pydbcapstone/output.tf
@@ -2,8 +2,8 @@ output "ecr_repo" {
   value = module.pydbcapstone.ecr_repo_name
 }
 
-resource "null_resource" "save_ecr_url" {
-  provisioner "local-exec" {
-    command = "ECR_REPO_URL=${module.pydbcapstone.ecr_repo_name}"
-  }
+
+resource "local_file" "write_url" {
+  content  = "ECR_REPO_URL=${module.pydbcapstone.ecr_repo_name}"
+  filename = "${path.module}/ecr_url.dat"
 }
