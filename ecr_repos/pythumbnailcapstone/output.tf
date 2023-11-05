d3382212497e96878026874be476d579fc8035b0
Low Chee Meng <lowcheemeng02@gmail.com>
2023-11-05 19:40:05 +0800

    update thumbnail repo pipeline

--- ecr_repos/pythumbnailcapstone/output.tf
+++ ecr_repos/pythumbnailcapstone/output.tf
@@ -1,3 +1,8 @@
 output "ecr_repo" {
   value = module.pythumbnailcapstone.ecr_repo_name
 }
+
+resource "local_file" "write_url" {
+  content  = "ECR_REPO_URL=${module.pythumbnailcapstone.ecr_repo_name}"
+  filename = "${path.module}/ecr_url.dat"
+}
